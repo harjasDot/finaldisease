c34b4e46ff44b7ddd542af0b35600ee92c6632be
Harjaspreet singh <singh.harjas2002@gmail.com>
2022-08-19 16:06:37 +0530

    runtime added

--- projectone/settings.py
+++ projectone/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=k9p-p=wn8b_8m*f!1_+a$(mm2=y#&0dhu1+2(qs(y2cc+h4$g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['https://multidiseasee.herokuapp.com/','*']
 
@@ -118,14 +118,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
-STATICFILES_DIRS=None
